9cb30699c7aac6d17e7504baaaa18193fa23a50e
saeedamini144 <saaed.amini@gmail.com>
2023-10-16 10:20:53 +0330

    change style

--- functions.php
+++ functions.php
@@ -14,19 +14,35 @@
         wp_enqueue_style('yogatheme-animate', get_template_directory_uri() . '/assets/css/animate.min.css', array() ,'1.0', 'all');
         wp_enqueue_style('yogatheme-lightbox', get_template_directory_uri() . '/assets/css/lightbox.min.css', array() , '1.0', 'all');
         wp_enqueue_style('yogatheme-splide', get_template_directory_uri() . '/assets/css/splide.min.css', array() , '1.0', 'all');
-    }
+    }   
     add_action('wp_enqueue_scripts' , 'yoga_register_style');
 
     function yoga_register_scripts(){
-        wp_enqueue_script('jquery');
-        wp_enqueue_script('yogatheme-splide' , get_template_directory_uri() . '/assets/javascripts/splide.min.js' . array('jquery') , '1.0');
-        wp_enqueue_script('yogatheme-bootstrapmin' , get_template_directory_uri() . '/assets/javascripts/bootstrap.min.js' . array('jquery') , '1.0');
-        wp_enqueue_script('yogatheme-bootstrapbundle' , get_template_directory_uri() . '/assets/javascripts/bootstrap.bundle.min.js' . array('jquery') , '1.0');
-        wp_enqueue_script('yogatheme-bootstrapesm' , get_template_directory_uri() . '/assets/javascripts/bootstrap.esm.min.js' . array('jquery') , '1.0');
-        wp_enqueue_script('yogatheme-lightbox' , get_template_directory_uri() . '/assets/javascripts/lightbox.min.js' . array('jquery') , '1.0');
-        wp_enqueue_script('yogatheme-owl' , get_template_directory_uri() . '/assets/javascripts/owl.carousel.min.js' . array('jquery') , '1.0');
-        wp_enqueue_script('yogatheme-wow' , get_template_directory_uri() . '/assets/javascripts/wow.min.js' . array('jquery') , '1.0');
-        wp_enqueue_script('yogatheme-yoga' , get_template_directory_uri() . '/assets/javascripts/js-yoga.js' . array('') , '1.0');
+        // wp_enqueue_script('jquery');
+        wp_enqueue_script('jquery', get_template_directory_uri() . '/assets/javascripts/jquery-3.6.0.js' ,  array(), '3.6.0', false);
+        wp_enqueue_script('yogatheme-splide' , get_template_directory_uri() . '/assets/javascripts/splide.min.js' , array() , '1.0' , false);
+        // wp_enqueue_script('yogatheme-splide' , 'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js' , array('jquery') , '1.0' , true);
+        wp_enqueue_script('yogatheme-bootstrapmin' , get_template_directory_uri() . '/assets/javascripts/bootstrap.min.js'  , array('jquery') , '1.0' , false);
+        wp_enqueue_script('yogatheme-bootstrapbundle' , get_template_directory_uri() . '/assets/javascripts/bootstrap.bundle.min.js' , array('jquery') , '1.0' , false);
+        wp_enqueue_script('yogatheme-bootstrapesm' , get_template_directory_uri() . '/assets/javascripts/bootstrap.esm.min.js' , array('jquery') , '1.0' , false);
+        wp_enqueue_script('yogatheme-lightbox' , get_template_directory_uri() . '/assets/javascripts/lightbox.min.js' , array('jquery') , '1.0' , false);
+        wp_enqueue_script('yogatheme-owl' , get_template_directory_uri() . '/assets/javascripts/owl.carousel.min.js' , array('jquery') , '1.0' , false);
+        wp_enqueue_script('yogatheme-wow' , get_template_directory_uri() . '/assets/javascripts/wow.min.js' , array('jquery') , '1.0' , false);
+        wp_enqueue_script('yogatheme-yoga' , get_template_directory_uri() . '/assets/javascripts/js-yoga.js' , array() , '1.0' , false);
     }
     add_action('wp_enqueue_scripts' , 'yoga_register_scripts');
+
+    // function yoga_register_scripts(){
+    //     wp_enqueue_script('jquery');
+    //     wp_enqueue_script('yogatheme-splide' , get_template_directory_uri() . '/assets/javascripts/splide.min.js' , array('jquery') , '1.0' , false);
+    //     wp_enqueue_script('yogatheme-bootstrap.min', get_template_directory_uri() . '/assets/javascripts/bootstrap.min.js' , array('jquery') ,"1.0" , true );
+    //     wp_enqueue_script('yogatheme-all.min', get_template_directory_uri() . '/assets/javascripts/all.min.js' , array('jquery') ,"1.0" , true );
+    //     wp_enqueue_script('yogatheme-bootstrap.bundle.min', get_template_directory_uri() . '/assets/javascripts/bootstrap.bundle.min.js' , array('jquery') ,"1.0" , true );
+    //     wp_enqueue_script('yogatheme-wow.min', get_template_directory_uri() . '/assets/javascripts/wow.min.js' , array('jquery') ,"1.0" , true );
+    //     wp_enqueue_script('yogatheme-splide.min', get_template_directory_uri() . '/assets/javascripts/splide.min.js' , array('jquery') ,"1.0" , true );
+    //     wp_enqueue_script('yogatheme-mainjs', get_template_directory_uri() . '/assets/javascripts/js-yoga.js' , array() ,"1.0" , true );
+    // }
+    // add_action("wp_enqueue_scripts", "yoga_register_scripts" );
+
+    
 ?>
