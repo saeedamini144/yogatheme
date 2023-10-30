2cef0eaccaac82124a459a47f72561fd19a0237a
saeedamini144 <saaed.amini@gmail.com>
2023-10-30 13:47:30 +0330

    add page style

--- page.php
+++ page.php
@@ -4,6 +4,7 @@
     <section class="header-banner">
         <div class="header-title ">
             <h2><?php the_title() ?></h2>
+            <!-- <p><?php the_time( 'F j, Y' );?></p> -->
         </div>
     </section>
     <section class="container mt-5 textsection">
