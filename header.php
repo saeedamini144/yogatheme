1a7b37e963ba6a1504d2344a603c7cf6335b10d0
saeedamini <42317098+saeedamini144@users.noreply.github.com>
2024-02-17 10:39:56 +0330

    Update header.php
    disable the silder in header.php

--- header.php
+++ header.php
@@ -64,7 +64,7 @@
         </div>
     </header>
 
-    <div class="splide" aria-label="Splide Basic HTML Example" >
+<!--     <div class="splide" aria-label="Splide Basic HTML Example" >
         <div class="splide__track">
             <ul class="splide__list">
                 <li class="splide__slide">
@@ -78,4 +78,4 @@
                 </li>
             </ul>
         </div>
-    </div>
+    </div> -->
